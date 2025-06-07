c6dda38a77fc0b5c3532e604635bfb34cea3e079
mairu0087 <jenishlinb@gmail.com>
2025-06-08 00:11:40 +0530

    adminpanel small update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,18 @@
-const mongoose=require("mongoose");
-const URI="mongodb+srv://m45928282:muskan%40143@salforddb.xlulqaq.mongodb.net/?retryWrites=true&w=majority"
-const connectdb=async(req,res)=>{
-    try{
-     await mongoose.connect(URI)
-     console.log("your database is connected successfully")
-     }
-    catch(error){
-    console.log(error.message)
-    }
-}
-module.exports=connectdb
+const mongoose = require("mongoose");
+
+const URI = "mongodb+srv://m45928282:muskan%40143@salforddb.xlulqaq.mongodb.net/?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
